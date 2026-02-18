236a4d110b664e754ca37aadcf6986ff7629f27b
Rahul107213 <rpprajapati1072@gmail.com>
2026-02-18 16:19:34 +0530

    Delete responsance.py

--- responsance.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Predicted_response(BaseModel):
-        predicted_churn: int = Field(..., description="Predicted churn status of the customer (0 for not churn, 1 for churn)")
-        
